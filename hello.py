b3de6675231209f53c124b95afdc3ee987e38827
Kal Bartal <118667094+KalBartal@users.noreply.github.com>
2023-02-19 17:23:23 +0000

    Update hello.py
    defined virtual environment packages

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
+import sys
+import logging
+import os
+
+# Add virtual environment packages to path
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+venv_dir = os.path.join(base_dir, 'antenv', 'lib', 'python3.9', 'site-packages')
+sys.path.insert(0, venv_dir)
+
 from flask import Flask
 
 app = Flask(__name__)
